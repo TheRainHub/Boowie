00e8bae2cec02e3e0879f4c385c8a267b03df212
kinkLY <mikhailplehinstj@gmail.com>
2025-09-05 23:38:24 +0200

    Initial local commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
+import './global.css';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View className='bg-slate-950 flex-1 items-center justify-center'>
+      <Text className='text-2xl text-red-500'>Hello Boowie App!</Text>
       <StatusBar style="auto" />
     </View>
   );
