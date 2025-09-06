7e25f72c566e072cee72d9b91ee2d4fabf8674b7
kinkly <theraintv94@gmail.com>
2025-09-07 00:08:25 +0200

    add tabs and root layout, global.css now render once

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,15 @@
-import '../../global.css';
 import { StatusBar } from 'expo-status-bar';
 import { View, FlatList } from 'react-native';
 
-import books from '../dummyBooks';
-import BookListItem from '../components/BookListItem';
+import books from '../../dummyBooks';
+import BookListItem from '../../components/BookListItem';
 
 export default function App() {
   return (
     <View className='bg-slate-800 flex-1 justify-center p-4 pt-20'>
       <FlatList
         data={books}
-        contentContainerClassName='gap-4'
+        contentContainerClassName='gap-5'
         renderItem={({ item }) => <BookListItem book={item} />}
       />
       
