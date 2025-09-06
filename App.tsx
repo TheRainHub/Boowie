88bb04769989ca0f1bba7ce335c0ac2bb32e7a8d
kinkly <mikhailplehinstj@gmail.com>
2025-09-06 19:15:46 +0200

    books and ListItem

--- App.tsx
+++ App.tsx
@@ -1,21 +1,17 @@
 import './global.css';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 
+import book from './src/dummyBooks';
+import BookListItem from './src/components/BookListItem';
+ 
 export default function App() {
   return (
-    <View className='bg-slate-950 flex-1 items-center justify-center'>
-      <Text className='text-2xl text-red-500'>Hello Boowie App!</Text>
-      <StatusBar style="auto" />
+  <View className='bg-slate-800 flex-1 justify-center p-4'>
+    <BookListItem book={book[0]}/>
+
+    <StatusBar style="auto" />
     </View>
   );
+ 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
